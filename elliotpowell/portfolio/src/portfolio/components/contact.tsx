86564ef1ad42354b6a0693e1de179354d890b74d
Elliot Powell <johnp8727@gmail.com>
2023-03-08 18:36:20 +0000

    Update contact page to give success message

--- elliotpowell/portfolio/src/portfolio/components/contact.tsx
+++ elliotpowell/portfolio/src/portfolio/components/contact.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+/* 
+The react Component for the Contact Form
+*/
+
+import React, { useState } from "react";
 import { useImmer } from "use-immer";
 import { useScroll } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
 import getCookie from "../util/getCookie";
 
 const csrftoken = getCookie("csrftoken");
 
 export default function Contact() {
-  const cScroll = useScroll();
+  const { width, height } = useThree((state) => state.size);
   const [form, setFrom] = useImmer({
     name: "",
     email: "",
@@ -14,6 +19,10 @@ export default function Contact() {
     message: "",
   });
 
+  const [error, setError] = useState<string>("");
+  const [status, setStatus] = useState<string>("");
+
+  // Handle submitting the form to the api via a push require
   const submit = (e: React.FormEvent) => {
     e.preventDefault();
     if (csrftoken === null) return false;
@@ -21,17 +30,41 @@ export default function Contact() {
       method: "POST",
       headers: { "X-CSRFToken": csrftoken },
       body: JSON.stringify(form),
-    }).then((x) => {
-      console.log(x);
-    });
+    })
+      .then((x) => x.json())
+      .then((m) => {
+        // handle alerting users to errors otherwise clear form on success and alert
+        if ("error" in m) {
+          setStatus("error");
+          setError(m.error);
+        } else {
+          setStatus("success");
+          setFrom({
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+          });
+        }
+      })
+      .catch((e) => {
+        console.log("error", e);
+      });
   };
 
   return (
     <>
       <div className="form">
         <h2>Contact Me</h2>
+        <div className="alert alert-danger" hidden={status !== "error"}>
+          {error}
+        </div>
+        <div className="alert alert-success" hidden={status !== "success"}>
+          Succesfully submitted form
+        </div>
         <form onSubmit={submit}>
           <input
+            required
             type="text"
             placeholder="Name"
             className="form-control form-control-sm"
@@ -43,6 +76,7 @@ export default function Contact() {
             }}
           />
           <input
+            required
             type="email"
             placeholder="Email"
             className="form-control form-control-sm"
@@ -54,6 +88,7 @@ export default function Contact() {
             }}
           />
           <input
+            required
             type="text"
             placeholder="Subject"
             className="form-control form-control-sm"
@@ -65,9 +100,10 @@ export default function Contact() {
             }}
           />
           <textarea
+            required
             placeholder="Message"
             className="form-control"
-            rows={cScroll.el.clientHeight <= 500 ? 3 : 5}
+            rows={height <= 500 ? 3 : 5}
             value={form.message}
             onChange={(e) => {
               setFrom((d) => {
@@ -78,10 +114,11 @@ export default function Contact() {
           <br />
           <input type="submit" className="form-control form-control-sm" />
         </form>
-        {(cScroll.el.clientHeight <= 500 || cScroll.el.clientWidth <= 600) && (
+        {/* If the screen is small we instread show various contact and social links below our form instead of side bar */}
+        {(height <= 500 || width <= 600) && (
           <>
             <br />
-            <ul className={`navbar-nav navbar-contact ${cScroll.el.clientHeight <= 550 && "list-group-horizontal"}`}>
+            <ul className={`navbar-nav navbar-contact ${height <= 550 && "list-group-horizontal"}`}>
               <li className="nav-item">
                 <a
                   className="nav-link"
@@ -91,14 +128,7 @@ export default function Contact() {
                     window.open("https://github.com/e11i0t23", "_blank");
                   }}
                 >
-                  <span
-                    className="sh-symbol--circle sh-symbol"
-                    style={{
-                      display: "inline-block",
-                      height: "14px",
-                      position: "relative",
-                    }}
-                  ></span>
+                  <span className="sh-symbol--circle sh-symbol sh-bullet"></span>
                   Github
                 </a>
               </li>
@@ -111,14 +141,7 @@ export default function Contact() {
                     window.open("mailto:elliot2810@gmail.com", "_blank");
                   }}
                 >
-                  <span
-                    className="sh-symbol--circle sh-symbol"
-                    style={{
-                      display: "inline-block",
-                      height: "14px",
-                      position: "relative",
-                    }}
-                  ></span>
+                  <span className="sh-symbol--circle sh-symbol sh-bullet"></span>
                   Email
                 </a>
               </li>
@@ -131,14 +154,7 @@ export default function Contact() {
                     window.open("/static/portfolio/public/elliot-powell-resume.pdf", "_blank");
                   }}
                 >
-                  <span
-                    className="sh-symbol--circle sh-symbol"
-                    style={{
-                      display: "inline-block",
-                      height: "14px",
-                      position: "relative",
-                    }}
-                  ></span>
+                  <span className="sh-symbol--circle sh-symbol sh-bullet"></span>
                   Resume
                 </a>
               </li>
